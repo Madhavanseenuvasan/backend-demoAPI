fa40b2d7ebbc2bad1f6ee43cfe3c4abe70b37973
Madhavan <madhavanseenu31@gmail.com>
2025-08-08 15:51:36 +0530

    updated db

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,8 +1,19 @@
 const mongoose=require('mongoose');
 
-const connectDataBase=()=>{
-  mongoose.connect(process.env.DB_URL).then((con)=>{
-    console.log('DataBase is connected to the '+con.connection.host);
-  })
-}
+// const connectDataBase=()=>{
+//   mongoose.connect(process.env.DB_URL).then((con)=>{
+//     console.log('DataBase is connected to the '+con.connection.host);
+//   })
+// }
+
+const connectDatabase = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(' MongoDB connection failed:', error.message);
+    process.exit(1);
+  }
+};
+
 module.exports=connectDataBase;
